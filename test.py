fb1f5825cb810176dc5a2dbc7cbcf49a17dfc122
EC2 Default User <ec2-user@ip-172-31-77-218.ap-south-1.compute.internal>
2023-08-17 08:56:59 +0000

    Added Code

--- test.py
+++ test.py
@@ -1,10 +1,59 @@
 from langchain.llms import OpenLLM
 
+from transformers import AutoModelForCausalLM
+from peft import LoraConfig, get_peft_model
+
+
+import torch
+
+MODEL_NAME = "tiiuae/falcon-7b"
+
+
+def langchain_llm():
+    print("Loading LLM...")
+    llm = OpenLLM(model_name=MODEL_NAME, model_id=MODEL_NAME)
+    print("Loaded Open Source LLM: ",llm.model_name)
+    with torch.cuda.amp.autocast():
+        print("In Autocast, Inferencing")
+        llm("What is the difference between a duck and a goose? And why there are so many Goose in Canada?")
+
+def qlora_llm():
+    
+    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, trust_remote_code=True, load_in_4bit=True)
+    print("Loaded Model", MODEL_NAME)
+    
+    
+    
+    # print("Trainable Params:")
+    # model.print_trainable_parameters()
+    # lora_config = LoraConfig(
+    #     r=8,
+    #     lora_alpha=32,
+    #     target_modules=[
+    #         "query_key_value",
+    #         "dense",
+    #         "dense_h_to_4h",
+    #         "dense_4h_to_h",
+    #     ],
+    #     lora_dropout=0.05,
+    #     bias="none",
+    #     task_type="CAUSAL_LM"
+    # )
+    # model = get_peft_model(model, lora_config)
+    # print("Trainable Params (After PEFT):")
+    # model.print_trainable_parameters()
+
 def main():
-    llm = OpenLLM(model_name="falcon", model_id='tiiuae/falcon-7b-instruct')
-
-    llm("What is the difference between a duck and a goose? And why there are so many Goose in Canada?")
-
+    qlora_llm()
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
